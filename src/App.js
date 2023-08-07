c2a19b4e33b0865fb2e0ff9fcde958c4ffae6efd
Kupz <benjohnvillabesa@gmail.com>
2023-08-07 11:53:29 +0800

    login-signup done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import { useState } from "react";
-import Logo from "./pages/global/Logo";
+
 import "./styles/main.scss";
 import Login from "./pages/Login";
+import Signup from "./Signup";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 function App() {
   window.document.title = "M1 Tracker v2";
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
+
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+      errorElement: <h1>Error: 404. Page Not Found!.</h1>,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+      errorElement: <h1>Error: 404. Page Not Found!.</h1>,
+    },
+  ]);
 
   return (
     <div className="App">
-      <Logo />
-      <Login />
+      {/* <Logo /> */}
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,29 @@
-import { Button, TextField } from "@mui/material";
+import logo from "../../assets/m1-logo.png";
+import someImage from "../../assets/bg.svg";
 
 export default function Login() {
   return (
     <div className="login">
-      <form>
-        <TextField variant="outlined" label="Email" type="email" />
-        <TextField variant="outlined" label="Password" type="password" />
-        <Button variant="outlined">Sign in</Button>
-      </form>
+      <div className="login-left-section">
+        <img src={someImage} alt="image" />
+      </div>
+      <div className="login-form-container">
+        {/* logo here */}
+        <img src={logo} alt="logo" className="logo" />
+        <h1>Hi, Welcome!</h1>
+        <p>Take charge. Be your Own Money Manager! </p>
+        <form>
+          <input type="email" placeholder="E-mail" />
+          <input type="password" placeholder="Password" />
+          <p>Forgot your passwod ?</p>
+          <button>Sign in</button>
+        </form>
+        <div>
+          <p>
+            don't have an account ? <a href="/signup">Sign up</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
